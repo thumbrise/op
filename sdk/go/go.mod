349865adc39cbe058da9d163475379773d80c110
Ruslan Kokoev <ruslan.kokoev.1999@gmail.com>
2026-04-20 16:41:09 +0300

    feat: add sdk for go, ts, php

---
+++ sdk/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/thumbrise/op/sdk/go
+
+go 1.26.0
---
+++ sdk/ts/op.ts
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2026 thumbrise
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Operation Protocol Instruction
+ */
+export interface Instruction {
+    version:    string;
+    operations: Operation[];
+}
+
+export interface Operation {
+    /**
+     * Machine-readable operation identifier. For example, full function name.
+     */
+    id:      string;
+    /**
+     * Human-readable comment about the operation.
+     */
+    comment: string;
+    input:   Term[];
+    output:  Term[];
+    errors:  Term[];
+    /**
+     * Ecosystem extensions. For expression problem resolving.
+     */
+    traits?: { [key: string]: any };
+}
+
+/**
+ * The atom of a rail.
+ * Kind is required when value or of is set.
+ */
+export interface Term {
+    id:     string;
+    kind?:  Kind;
+    /** Requires kind. */
+    value?: boolean | number | string;
+    /** Requires kind. */
+    of?:    Term[];
+}
+
+export enum Kind {
+    Array = "array",
+    Binary = "binary",
+    Boolean = "boolean",
+    Datetime = "datetime",
+    Enum = "enum",
+    Float = "float",
+    Integer = "integer",
+    Object = "object",
+    String = "string",
+}
