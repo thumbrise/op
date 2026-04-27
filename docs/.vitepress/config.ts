c794868b24856996b6d77660816241bcf9e04440
Ruslan Kokoev <ruslan.kokoev.1999@gmail.com>
2026-04-27 15:51:35 +0300

    wip schema

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -47,7 +47,8 @@ export default withMermaid(defineConfig({
       {text: 'Universal', link: '/universal/'},
       {text: "Tim's Dream", link: '/dreams/'},
       {text: 'Reference', link: '/reference/rfc-operation-protocol'},
-      {text: 'Schema', link: '/schema/instruction.v1.json'},
+      // Specific link case
+      {text: 'Schema', link: '/op/schema/instruction.v1.json'},
       {text: 'GitHub', link: 'https://github.com/thumbrise/op'},
     ],
 
