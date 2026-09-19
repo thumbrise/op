f31bb4cc2d63576e9e81bd494a6cf6c20f336bcd
Ruslan Kokoev <ruslan.kokoev.1999@gmail.com>
2026-09-19 14:37:04 +0300

    feat: Add metrics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,10 +4,16 @@ import Playground from '../components/Playground.vue'
 import TermEditor from '../components/TermEditor.vue'
 import TermViz from '../components/TermViz.vue'
 import SciTooltip from '../components/SciTooltip.vue'
+import {yandexMetrika} from "@hywax/vitepress-yandex-metrika";
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({app}) {
+  enhanceApp({app, router}) {
+    yandexMetrika(router, {
+      counter: {
+        id: 112814193
+      },
+    })
     app.component('Playground', Playground)
     app.component('TermEditor', TermEditor)
     app.component('TermViz', TermViz)
